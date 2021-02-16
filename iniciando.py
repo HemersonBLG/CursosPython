e6ee01e3d4c83677a9ef17be739a09290ebae92d
hemersonblg <hemerson.blg@gmail.com>
2021-02-15 22:49:28 -0300

    if and elses

--- iniciando.py
+++ iniciando.py
@@ -1,5 +1,9 @@
 import os
 nome = input('Digite seu nome: ')
-idade = input('Digite sua idade: ')
+idade = int(input('Digite sua idade: '))
 
 print('Prazer {} sua idade eh: {}'.format(nome, idade))
+if idade >= 25:
+    print('Ce ta vei hein!')
+else:
+    print('ce eh novim d+!')
