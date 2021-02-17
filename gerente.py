51ae837002f41264fe0a48d12d9ae63dd58ef7f8
hemersonblg <hemerson.blg@gmail.com>
2021-02-17 14:33:19 -0300

    starting project

---
+++ gerente.py
@@ -0,0 +1,9 @@
+from funcionario import Funcionario
+
+
+class Gerente(Funcionario):
+
+    def __init__(self, nome, endereco, cpf, data_nascimento, cargo, matricula, senha, salario, qtd_funcionarios):
+        super().__init__(nome, endereco, cpf, data_nascimento,
+                         cargo, matricula, senha, salario)
+        self._qtd_funcionarios = qtd_funcionarios
